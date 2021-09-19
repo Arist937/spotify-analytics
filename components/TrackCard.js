3d6d9874a1cf8d28f7523e08e902f8eb0ceba9bd
Arist937 <Aristotle937@gmail.com>
2021-09-19 19:06:59 -0400

    added top 50 artist functionality

--- components/TrackCard.js
+++ components/TrackCard.js
@@ -2,11 +2,13 @@ import React from "react";
 import styles from "./styles/trackCard.module.css";
 import Image from "next/image";
 
-const TrackCard = ({ name, picture }) => {
+const TrackCard = ({ name, picture, position }) => {
   return (
     <div className={styles.container}>
       <Image src={picture} alt="album cover" width={100} height={100} />
-      <p className={styles.name}>{name}</p>
+      <p className={styles.name}>
+        {position}. {name}
+      </p>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,9 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const [signedIn, setSignedIn] = useState(false);
   const [displayName, setDisplayName] = useState("");
+  const [category, setCategory] = useState("Tracks");
   const [topTracks, setTopTracks] = useState(null);
+  const [topArtists, setTopArtists] = useState(null);
 
   useEffect(() => {
     const access_token = new URLSearchParams(window.location.hash).get(
@@ -26,8 +28,8 @@ export default function Home() {
         .then((response) => {
           const res = response.data;
           setDisplayName(res.display_name);
-        });
-
+        })
+        .catch((err) => console.log(err));
       axios
         .get("https://api.spotify.com/v1/me/top/tracks", {
           headers: {
@@ -40,46 +42,115 @@ export default function Home() {
         })
         .then((response) => {
           const res = response.data;
-          console.log(res);
           setTopTracks(response.data.items);
+        })
+        .catch((err) => console.log(err));
+      axios
+        .get("https://api.spotify.com/v1/me/top/artists", {
+          headers: {
+            Authorization: "Bearer " + access_token, //the token is a variable which holds the token
+          },
+          params: {
+            time_range: "long_term",
+            limit: "50",
+          },
+        })
+        .then((response) => {
+          const res = response.data;
+          console.log(res);
+          setTopArtists(response.data.items);
         })
         .catch((err) => console.log(err));
     }
   }, []);
 
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Spotify Analytics</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {signedIn ? (
-        <>
-          <h1 className={styles.title}>{displayName}</h1>
-          <div
-            style={{
-              display: "grid",
-              gridTemplateColumns: "auto auto auto auto auto",
-              gap: "20px 20px",
-              padding: 10,
-            }}
-          >
-            {topTracks &&
-              topTracks.map((track) => (
-                <TrackCard
-                  key={track.id}
-                  picture={track.album.images[0].url}
-                  name={track.name}
-                />
-              ))}
+        <div className={styles.signedInContainer}>
+          <h1 className={styles.title}>
+            Welcome to Spotify Analytics, {displayName}
+          </h1>
+          <div>
+            <button
+              className={styles.tabs}
+              onClick={() => {
+                setCategory("Tracks");
+              }}
+            >
+              Top Tracks
+            </button>
+            <button
+              className={styles.tabs}
+              onClick={() => {
+                setCategory("Artists");
+              }}
+            >
+              Top Artists
+            </button>
+            <button
+              className={styles.tabs}
+              onClick={() => {
+                setCategory("Upload");
+              }}
+            >
+              Upload Data File
+            </button>
           </div>
-        </>
+          {category === "Tracks" ? (
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "auto auto auto auto auto",
+                gap: "20px 20px",
+                padding: 10,
+              }}
+            >
+              {topTracks &&
+                topTracks.map((track, index) => (
+                  <TrackCard
+                    key={track.id}
+                    position={index + 1}
+                    picture={track.album.images[0].url}
+                    name={track.name}
+                  />
+                ))}
+            </div>
+          ) : category === "Artists" ? (
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "auto auto auto auto auto",
+                gap: "20px 20px",
+                padding: 10,
+              }}
+            >
+              {topArtists &&
+                topArtists.map((artist, index) => (
+                  <TrackCard
+                    key={artist.id}
+                    position={index + 1}
+                    picture={artist.images[0].url}
+                    name={artist.name}
+                  />
+                ))}
+            </div>
+          ) : (
+            <div style={{ padding: 10 }}>
+              <h1 className={styles.title}>This Feature is Coming Soon</h1>
+            </div>
+          )}
+        </div>
       ) : (
-        <>
-          <h1 className={styles.title}>Welcome to Spotify Analytics</h1>
+        <div className={styles.defaultContainer}>
+          <h1 className={styles.title}>Spotify Analytics</h1>
           <SignInButton />
-        </>
+        </div>
       )}
-    </div>
+    </>
   );
 }
